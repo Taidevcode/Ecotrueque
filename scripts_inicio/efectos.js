8e0b93d47114f7b62c3ae0276a827f7dfdc86d84
Thaiel <thaiel.cuellar@gmail.com>
2024-05-14 02:29:21 -0300

    actualizacion

--- scripts_inicio/efectos.js
+++ scripts_inicio/efectos.js
@@ -7,3 +7,29 @@ window.sr = ScrollReveal();
     sr.reveal('.section-slider', {
         duration: 1500
     });
+
+    sr.reveal('.banner', {
+        duration: 1500
+    });
+    
+    sr.reveal('.container-categorias', {
+        duration: 1500
+    });
+
+    
+    sr.reveal('.container-products', {
+        duration: 1500
+    });
+
+    
+    sr.reveal('.container-blogs', {
+        duration: 1500
+    });
+
+    sr.reveal('.container-general', {
+        duration: 1500
+    });
+
+    sr.reveal('.container-nosotros', {
+        duration: 1500
+    });
